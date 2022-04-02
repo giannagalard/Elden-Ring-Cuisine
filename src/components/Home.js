dcea32d0b6bc7686bb8ba38ab3a2549fcd13afab
giannagalard <giannagalard@gmail.com>
2022-04-02 13:02:21 -0400

    FEAT: Home template finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Fragment } from "react";
 import "../styles/home.css";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Fragment>
+      <div className="header">
+        <h1>The Fellowship of the Elden Ring</h1>
+      </div>
+      <div className="subHeader">
+        <h2>Stock Trading and Investing</h2>
+      </div>
+      <div className="image">
+        <img
+          className="stockImage"
+          src="https://data.whicdn.com/images/354770748/original.jpg"
+          alt="image"
+        />
+      </div>
+      <div className="body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          dictum sodales rutrum. Vivamus ut risus et tortor tincidunt tincidunt
+          gravida ut nisl. Pellentesque justo est, feugiat eget augue quis,
+          convallis lacinia quam. Curabitur interdum sodales velit vitae
+          pulvinar. Nam ultrices quam vitae pulvinar porta. Vivamus cursus
+          porttitor venenatis. Cras gravida urna quam, quis egestas diam blandit
+          eu. Maecenas euismod nisl tortor, vel laoreet tortor efficitur vel.
+          Nulla dictum viverra elit eget finibus. Aliquam finibus turpis odio,
+          sit amet volutpat ex fringilla non. Nunc vel sollicitudin purus.
+        </p>
+      </div>
+    </Fragment>
+  );
 }
-
