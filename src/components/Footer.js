aacbcf0284d7ebbbb16003e3ec554bd8c257a115
giannagalard <giannagalard@gmail.com>
2022-04-02 15:02:37 -0400

    FEAT: Finished Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <Fragment>
+      <footer
+        className=" text-center text-white"
+        style={{ backgroundColor: "#84719b" }}
+      >
+        <div className="container p-4"></div>
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2022 Copyright:
+          <a className="text-white" href="#">
+            The Fellowship of the Elden Ring
+          </a>
+        </div>
+      </footer>
+    </Fragment>
+  );
 }
