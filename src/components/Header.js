c64a7d32cc6d1305227ade9af04d5bf1fa955501
giannagalard <giannagalard@gmail.com>
2022-04-02 12:47:47 -0400

    FEAT: Completed nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import "../styles/header.css";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <Fragment>
+      <nav
+        class="navbar navbar-expand-lg navbar-dark"
+        style={{ backgroundColor: " #84719B" }}
+      >
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            The Fellowship of the Elden Ring
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                  Stocks
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Settings
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Security
+                    </a>
+                  </li>
+                  <hr class="dropdown-divider" />
+
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      insert other if needed
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-dark" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </Fragment>
+  );
 }
