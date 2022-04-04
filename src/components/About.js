0d1a60ec917f5f67cca36bf274b028aa303f94c3
giannagalard <giannagalard@gmail.com>
2022-04-03 23:25:43 -0400

    resolved errors

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,24 @@
 import React, { Fragment } from "react";
 import paper from "../images/paper.png";
+import "../styles/about.css";
 
 export default function About() {
   return (
     <Fragment>
-      <div className="aboutTitle">
-        <h1>About Us</h1>
-      </div>
-      <div className="image">
-        <img className="paper" src={paper} alt="paper" />
-      </div>
-      <div className="aboutText">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget
-          egestas nisl nisi eu ipsum. Nulla facilisi. Nulla facilisi.
-          Pellentesque euismod, urna eu tincidunt consectetur, nisi nisl aliquam
-          nunc, eget egestas nisl nisi eu ipsum. Nulla facilisi. Nulla facilisi.
-        </p>
+      <div style={{ paddingBottom: "2.5rem" }}>
+        <div className="image">
+          <img className="paper" src={paper} alt="paper" />
+        </div>
+        <div className="aboutText">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque euismod, urna eu tincidunt consectetur, nisi nisl
+            aliquam nunc, eget egestas nisl nisi eu ipsum. Nulla facilisi. Nulla
+            facilisi. Pellentesque euismod, urna eu tincidunt consectetur, nisi
+            nisl aliquam nunc, eget egestas nisl nisi eu ipsum. Nulla facilisi.
+            Nulla facilisi.
+          </p>
+        </div>
       </div>
     </Fragment>
   );
