1f5bdebeeca2701641d7ba0defa18788a97603bb
giannagalard <giannagalard@gmail.com>
2022-04-04 19:47:25 -0400

    FEAT: FULLY RESPONSIVE ABOUT PAGE

--- src/components/About.js
+++ src/components/About.js
@@ -14,28 +14,34 @@ export default function About() {
             </div>
           </Hidden>
         </Hidden>
-        {/* <div className="aboutHeader">
-            <h1>About Us</h1>
-          </div>
-          <div className="aboutText">
-            <p>
-              The Elden Ring Restaurant has been around for a very long time,
-              even during the reign of the first elden lord. Our business is
-              spread all across the Lands Between having one restaurant in each
-              area. Limgrave, Caelid, Liurnia of the Lakes, Atlus Plateau, Mt.
-              Gelmir, Mountaintops of the Giants, and The River Kingdoms. These
-              are the areas where thy restaurants lies. We welcome anyone, the
-              living, the undead, the spirits, and even the maidenless. Any soul
-              with a rune to spare may rejoice in our generous establishment.
-              Our consumables are one of a kind, it's as if you've received the
-              blessing from thy Erdtree. Even the tarnished could feel like
-              they've reclaimed their grace once they've had a bite of our food.
-              Not only do we have the best stuff here, we have plenty of maidens
-              serving here. What is thou waiting for? Come and visit any of our
-              establishments!
-            </p>
-          </div> */}
       </div>
+      <Hidden lgUp>
+        <div className="aboutHeader">
+          <h1>About Us</h1>
+        </div>
+        <div style={{ paddingBottom: "2.5rem" }}>
+          <div className="aboutTextContainer">
+            <div className="aboutText">
+              <p>
+                The Elden Ring Restaurant has been around for a very long time,
+                even during the reign of the first elden lord. Our business is
+                spread all across the Lands Between having one restaurant in
+                each area. Limgrave, Caelid, Liurnia of the Lakes, Atlus
+                Plateau, Mt. Gelmir, Mountaintops of the Giants, and The River
+                Kingdoms. These are the areas where thy restaurants lies. We
+                welcome anyone, the living, the undead, the spirits, and even
+                the maidenless. Any soul with a rune to spare may rejoice in our
+                generous establishment. Our consumables are one of a kind, it's
+                as if you've received the blessing from thy Erdtree. Even the
+                tarnished could feel like they've reclaimed their grace once
+                they've had a bite of our food. Not only do we have the best
+                stuff here, we have plenty of maidens serving here. What is thou
+                waiting for? Come and visit any of our establishments!
+              </p>
+            </div>
+          </div>
+        </div>
+      </Hidden>
     </Fragment>
   );
 }
