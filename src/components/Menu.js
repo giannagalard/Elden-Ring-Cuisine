5fab60be60dcb195370cceec9721358413519797
giannagalard <giannagalard@gmail.com>
2022-04-08 14:35:16 -0400

    CHORE: fix menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,13 @@
 import React, { Fragment } from "react";
+import menu from "../images/menu.svg";
+import "../styles/menu.css";
 
 export default function Menu() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className="menuImage">
+        <img className="menu" src={menu} alt="menu" />
+      </div>
+    </Fragment>
+  );
 }
