812211d78e6123f2bd62993e5f451ffff14a3a99
giannagalard <giannagalard@gmail.com>
2022-04-09 09:58:50 -0400

    FEAT: WORKING MENU

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
 import menu from "../images/menu.svg";
 import "../styles/menu.css";
+// import menu from "../images/menu.png";
 
 export default function Menu() {
   return (
     <Fragment>
       <div className="menuImage">
-        <img className="menu" src={menu} alt="menu" />
+        <img className="menuSize" src={menu} alt="menu" />
       </div>
+      <div className="menuDivider"></div>
     </Fragment>
   );
 }
